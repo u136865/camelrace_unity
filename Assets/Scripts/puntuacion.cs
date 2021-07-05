592a41b9ad1051121bef519f74162de387941830
MurkyStyle <71457400+MurkyStyle@users.noreply.github.com>
2021-07-05 13:38:58 +0200

    Se me oye?
    Ey, cómo va eso? confirmad si habéis podido descargar esta versión.

--- Assets/Scripts/puntuacion.cs
+++ Assets/Scripts/puntuacion.cs
@@ -1,24 +1,45 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class puntuacion : MonoBehaviour
 {
-    public TMP_Text puntos, pt1, pt2, pt3;
-    public static int total, punt1, punt2, punt3;
+    public TMP_Text puntos, pt1, pt2, pt3, textTiempo, textTiempoFinal;
+    public static int total, punt1, punt2, punt3, totalMovimientoCamello;
+    public int multiplicadorCamello = 3;
+    public RectTransform camello;
+    public GameObject pantallaWin;
+    
+    float segundosTiempo = 0, camelloMovimiento = 450; //El camello va del 450 de x al -450 de x para llegar a la meta   
+    int minutosTiempo = 0;    
+    //int horasTiempo = 0;
 
     void Start()
     {
         puntos.GetComponent<TextMeshProUGUI>().text = "0";
+        camello = camello.GetComponent<RectTransform>();
     }
 
     private void Update()
     {
+        TiempoJuego();
+        MoverCamello();
         puntos.GetComponent<TextMeshProUGUI>().text = total.ToString();
         pt1.GetComponent<TextMeshProUGUI>().text = punt1.ToString();
         pt2.GetComponent<TextMeshProUGUI>().text = punt2.ToString();
         pt3.GetComponent<TextMeshProUGUI>().text = punt3.ToString();
+        camello.localPosition = new Vector3(camelloMovimiento, camello.localPosition.y, camello.localPosition.z);
+    }
+    void TiempoJuego()
+    {
+        segundosTiempo += Time.deltaTime;
+        minutosTiempo = (int)segundosTiempo / 60;
+       // horasTiempo = (int)segundosTiempo / 3600;
+        //UI Text refresh
+        textTiempo.text = string.Format("{0}:{1}",  minutosTiempo.ToString("00"), ((int)segundosTiempo % 60).ToString("00"));//{0}:{1}:{2} horasTiempo.ToString("00"),
     }
 
     public static void MeterPuntos(int punto)
@@ -33,5 +54,35 @@ public class puntuacion : MonoBehaviour
         total += punto;
     }
 
+    void MoverCamello ()
+    {
+       int mover = 450 - ((punt1 + punt2*2 + punt3*3) * multiplicadorCamello);
+       camelloMovimiento = mover;
+        if (camelloMovimiento <= -450)
+        {
+            PauseGame();
+            Winner();
+        }
+    }
+
+    void PauseGame()
+    {
+        Time.timeScale = 0;
+    }
+    void Winner()
+    {
+        Debug.Log("Hemos ganado!!!!");
+        pantallaWin.SetActive(true);
+        textTiempoFinal.text = textTiempo.text;
+    }
+
+    void ResumeGame()
+    {
+        Time.timeScale = 1;
+    }
+    public void Restart()
+    {
+        SceneManager.LoadScene("Juego");
+    }
 
 }
